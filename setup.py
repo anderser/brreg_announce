48909de3cc129c192940ff9daaeddc71446eb5f6
Anders Eriksen <anders.eriksen@bt.no>
2017-01-17 13:01:26 +0100

    New version in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='brreg_announce',
-      version='0.1',
+      version='0.2',
       description='This app scrapes the annoncements section (kunngjoringer) of Bronnoysundregistrene and returns the data on a structured format.',
       url='http://github.com/anderser/brreg_announce',
       author='anderser',
